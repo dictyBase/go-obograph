a351b8b2a734bf0313796ae6446664f038e2d969
Siddhartha Basu <biosidd@gmail.com>
2018-09-24 21:23:51 +0000

    refactor: update SaveOrUpdateTerms interface to return count of both inserted and updated terms

--- storage/storage.go
+++ storage/storage.go
@@ -17,8 +17,8 @@ type DataSource interface {
 	SaveTerms(graph.OboGraph) (int, error)
 	// UpdateTerms update existing terms in the storage
 	UpdateTerms(graph.OboGraph) (int, error)
-	// SaveorUpdateTerms either insert or update terms
-	SaveOrUpdateTerms(graph.OboGraph) (int, error)
+	// SaveorUpdateTerms either insert or update terms in the storage
+	SaveOrUpdateTerms(graph.OboGraph) (int, int, error)
 	// SaveRelationships persist all relationships in the storage
 	SaveRelationships(graph.OboGraph) (int, error)
 	// SaveNewRelationships skips the existing one and saves only the new relationships
