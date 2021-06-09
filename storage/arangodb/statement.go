b1f774029c3b16a5955fcd6a62b1734c960bc210
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-06-08 20:39:52 -0500

    refactor: use bind parameters for term update

--- storage/arangodb/statement.go
+++ storage/arangodb/statement.go
@@ -30,41 +30,41 @@ const (
 		FOR diff IN MINUS(latest,existing)
 			FOR cvt IN @@temp_collection
 				FILTER diff == cvt.id
-				INSERT UNSET(cvt,["_key","_id","_rev"]) IN @term_collection
+				INSERT UNSET(cvt,["_key","_id","_rev"]) IN @@term_collection
 				COLLECT WITH COUNT INTO c
 				RETURN c
 	`
 	tupdt = `
 		LET fcv = (
-		    FOR cv in %s
-		        FILTER cv.id == "%s"
-		        RETURN cv._id
+			FOR cv IN @@graph_collection
+				FILTER cv.id == @graph_id
+				RETURN cv._id
 		)
 		LET existing = (
-	        FOR cvt in %s
-	            FILTER fcv[0] == cvt.graph_id
-	            RETURN cvt.id
+				FOR cvt IN @@term_collection
+					FILTER fcv[0] == cvt.graph_id
+					RETURN cvt.id
 		)
 		LET latest = (
-			    FOR cvt in %s
-		        FILTER fcv[0] == cvt.graph_id
-		        RETURN cvt.id
+			FOR cvt IN @@temp_collection
+				FILTER fcv[0] == cvt.graph_id
+				RETURN cvt.id
 		)
 		FOR ins in INTERSECTION(latest,existing)
-			    FOR lcvt in %s
-			        FOR ecvt in %s
-						FILTER lcvt.graph_id == fcv[0]
-						FILTER ecvt.graph_id == fcv[0]
-		                FILTER ins == lcvt.id
-		                FILTER ins == ecvt.id
-		                UPDATE {
-		                     _key: ecvt._key,
-		                      label: lcvt.label,
-		                      metadata: lcvt.metadata
-	                   } IN %s
-	                   COLLECT WITH COUNT INTO c
-	                   RETURN c
-		`
+			FOR lcvt in @@temp_collection
+				FOR ecvt in @@term_collection
+					FILTER lcvt.graph_id == fcv[0]
+					FILTER ecvt.graph_id == fcv[0]
+					FILTER ins == lcvt.id
+					FILTER ins == ecvt.id
+					UPDATE {
+						 _key: ecvt._key,
+						  label: lcvt.label,
+						  metadata: lcvt.metadata
+				   } IN @@term_collection
+				   COLLECT WITH COUNT INTO c
+				   RETURN c
+	`
 	rinst = `
 		FOR c IN %s
 		    FOR cvt IN %s
