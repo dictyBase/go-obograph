8181bbab83493d9dd0342fcb9ce74d5c8a95298f
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-06-16 17:20:44 -0500

    fix: fix bunch of stylecheck and errcheck errors

--- generate/rand.go
+++ generate/rand.go
@@ -2,20 +2,24 @@ package generate
 
 import (
 	"crypto/rand"
+	"fmt"
 )
 
-func stringWithCharset(strSize int) string {
+func stringWithCharset(strSize int) (string, error) {
 	charSet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	ls := len(charSet)
 	b := make([]byte, strSize)
-	rand.Read(b)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", fmt.Errorf("error in reading random data %s", err)
+	}
 	for k, v := range b {
 		b[k] = charSet[v%byte(ls)]
 	}
-	return string(b)
+	return string(b), nil
 }
 
 // RandString generates a random string of a specific length
-func RandString(length int) string {
+func RandString(length int) (string, error) {
 	return stringWithCharset(length)
 }
