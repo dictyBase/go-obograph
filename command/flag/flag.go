8aa45b0f0cd79a4290ccca8fa00034d47eb1b2d3
Siddhartha Basu <biosidd@gmail.com>
2023-08-01 18:17:51 -0500

    feat(flag.go): add OntologyFlagsOnly function to return ontology-related flags only
    The OntologyFlagsOnly function is added to the flag.go file. This
    function returns a slice of cli.Flag that contains only the flags
    related to ontology.

--- command/flag/flag.go
+++ command/flag/flag.go
@@ -5,6 +5,33 @@ import (
 	"github.com/urfave/cli"
 )
 
+// OntologyFlagsOnly returns a slice of cli.Flag objects representing command line
+// options for an ontology-related CLI application.
+func OntologyFlagsOnly() []cli.Flag {
+	return []cli.Flag{
+		cli.StringFlag{
+			Name:  "term-collection",
+			Usage: "arangodb collection for storing ontoloy terms",
+			Value: "cvterm",
+		},
+		cli.StringFlag{
+			Name:  "rel-collection",
+			Usage: "arangodb collection for storing cvterm relationships",
+			Value: "cvterm_relationship",
+		},
+		cli.StringFlag{
+			Name:  "cv-collection",
+			Usage: "arangodb collection for storing ontology information",
+			Value: "cv",
+		},
+		cli.StringFlag{
+			Name:  "obograph",
+			Usage: "arangodb named graph for managing ontology graph",
+			Value: "obograph",
+		},
+	}
+}
+
 // OntologyFlags returns a cli.flag slice to use in the command
 // line arguments of the ontology loader.
 func OntologyFlags() []cli.Flag {
